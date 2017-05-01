0ac9ce013943365174115c04998060949b1a3aa9
rachel <yoonsy@hep.re.kr>
2017-05-01 15:27:30 +0900

    Admin Model Process Complete

--- haw-common/src/main/java/kr/re/hep/dao/MissionDao.java
+++ haw-common/src/main/java/kr/re/hep/dao/MissionDao.java
@@ -1,5 +1,43 @@
 package kr.re.hep.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.re.hep.vo.HintVO;
+import kr.re.hep.vo.MissionVO;
+
+@Repository("missionDAO")
 public class MissionDao {
 
+	private String namespace = "missionMapper.";
+	
+	@Autowired
+	SqlSessionTemplate session;
+	
+	public List<MissionVO> missionSelectAll(int quest_no){
+		return session.selectList(namespace +"missionSelectAll", quest_no);
+	}
+	
+	public MissionVO missionSelect(int mission_no){
+		return session.selectOne(namespace +"missionSelect", mission_no);
+	}
+	
+	public void missionDelete(int mission_no){
+		session.delete(namespace +"missionDelete", mission_no);
+	}
+	
+	public void missionSeqUpdateAll(MissionVO vo){
+		session.update(namespace +"missionSeqUpdateAll", vo);
+	}
+	
+	public int hintInsert(HintVO vo){
+		return session.insert(namespace +"hintInsert", vo);
+	}
+	
+	public void hintDelete(int hint_no){
+		session.delete(namespace +"hintDelete", hint_no);
+	}
 }
